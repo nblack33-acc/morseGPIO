ddfdfb949d2164f3e0df1e63de34f80826eccfa1
nblack33-acc <97971711+nblack33-acc@users.noreply.github.com>
2022-04-08 05:46:42 -0400

    Commented the file
    I added comments to explain program functionality and adjusted the units to be slower

--- gpiomorse.py
+++ gpiomorse.py
@@ -3,10 +3,12 @@
 from gpiozero import LED
 from time import sleep
 
+# Here you can set the basic parameters you want to work with
 led = LED(18)
-unit = 0.1
-message = "hello my name is not jeremy"
+unit = 0.3 # Unit of time you want messages to transmit in seconds. E.g. 0.1 seconds on/off/pause etc.
+message = "hello my name is not jeremy"	# The message, should be in plaintext, lower case, no punctuation (unless you expand the dict)
 
+# Both dit and dah include the sleep function at one unit, so no inter/intra character pauses are necessary
 def dit():
 	print("dit")
 	led.on()
@@ -21,6 +23,7 @@ def dah():
 	led.off()
 	sleep(unit)
 
+# For both word and character sleeps, they are reduced by one, since the dit and dah functions already sleep for 1 unit
 def cSpace():
 	print(" ")
 	sleep(unit*2)
@@ -29,6 +32,7 @@ def wSpace():
 	print("\n")
 	sleep(unit * 6)
 
+# The letterdict maps the morse code for each letter. It can be expanded to support any character, including unicode and emoji (to represent acronyms)
 letterDict = {
 	"a" : "._",
 	"b" : "_...",
@@ -58,6 +62,7 @@ letterDict = {
         "z" : "__..",
 }
 
+# The writeMessage function loops through each message character, then through the morse representation from the dict, and creates the appropriate output
 def writeMessage():
 	for letter in message:
 		if not letter.isspace():
@@ -68,7 +73,7 @@ def writeMessage():
 					dah()
 			cSpace()
 		else:
-			wSpace()
+			wSpace() # If the character is a space, it runs the wordspace function
 	print("\n")
 
 while True:
